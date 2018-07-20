591fa6c66467af800390f9a95ecef3ac8f212394
Irene Ma <iring.ma@gmail.com>
2018-07-20 13:36:40 -0700

    add world map to application page and initialize in javascript

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :destinations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'pages#home'
+  root to: 'pages#index'
   get 'about', to: 'pages#about'
+  get 'home', to: 'pages#home'
 
   resources :articles
 
