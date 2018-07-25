e1c70a2e5bea2357bf21323118477130378eb868
Irene Ma <iring.ma@gmail.com>
2018-07-25 11:54:10 -0700

    remove belongs to has one in country destinations and change title in destination creation to picklist

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,6 +1,5 @@
 class Destination < ApplicationRecord
   has_many :articles
-  belongs_to :country
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 end
